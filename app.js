b8009e836fc1351c815d857d531cde23b6ee58d1
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-24 12:50:57 -0800

    successfully grabbed list of songs from supabase

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 /* Imports */
-
+import { getSongs } from './fetch-utils.js';
+import { } from './render-utils.js';
 /* Get DOM Elements */
+const songListContainer = document.querySelector('song-list-container');
 
 /* State */
+let songsData = [];
 
 /* Events */
 
+window.addEventListener('load', async () => {
+    const songs = await getSongs();
+
+    songsData = songs;
+//display function?
+    // for (let song of songsData) {
+    //     - redner a div for each song
+    //     - append the div to the html element in dom
+    // - refactor render function
+    // - refactor a display function
+    // }
+    // console.log (songsData);
+});
+
+
+
+            
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
